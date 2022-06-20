93fdb3022d34d00f769191307229c54c3ad528d7
Cristopher J <cljarami@espol.edu.ec>
2022-06-20 14:41:17 -0500

    mi primer comentario

---
+++ scripts/ejercicio.js
@@ -0,0 +1,9 @@
+/* ejecutarCodigo => {
+    alert('Hola, mundo!');
+}
+
+ejecutarCodigo; */
+
+document.getElementById('titulo1') = "Album de fotos";
+document.getElementsByClassName('text-muted').innerHTML = "<span> En este sitio encontrarás un album de fotos inspirado en el snippet de <a href="https://codepen.io/taj1uddin/pen/eYVrLKy">Codepen - Taj Uddin</a>.</span>";
+document.getElementsByTagName('p').class = "d-none";
