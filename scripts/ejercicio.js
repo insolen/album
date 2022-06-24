bcf532e6dc1150fbcad6653e17a2d55ce41f547a
Cristopher J <cljarami@espol.edu.ec>
2022-06-24 16:56:03 -0500

    Primer commit

--- scripts/ejercicio.js
+++ scripts/ejercicio.js
@@ -1,7 +1,9 @@
-/* ejecutarCodigo => {
-    alert('Hola, mundo!');
-}
 
-ejecutarCodigo; */
+document.getElementById('titulo1').textContent = 'Album de fotos';
+document.getElementsByClassName('text-muted').innerHTML = '<span> En este sitio encontrarás un album de fotos inspirado en el snippet de <a href="https://codepen.io/taj1uddin/pen/eYVrLKy">Codepen - Taj Uddin</a>.</span>';
+document.getElementsByTagName('p')[2].setAttribute('class', 'd-none');
 
-document.documentElement.
+const div = document.querySelector("svg");
+const span = document.createElement("img");
+span.innerHTML = "hello james";
+div.replaceWith(span);
